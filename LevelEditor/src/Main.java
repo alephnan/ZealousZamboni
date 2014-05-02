91dab54e7cbcb7fe1aa0ec3b39c36b1b50d0c78b
danava04 <danava04@cs.washington.edu>
2014-05-01 21:23:34 -0700

    added functionality to choose tilemap width and height, added README that describes how to use SimpleLevelEditor.jar

--- LevelEditor/src/Main.java
+++ LevelEditor/src/Main.java
@@ -5,33 +5,31 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-
+/**
+ * A simple GUI to create levels for Zealous Zamboni.
+ * @author Dana
+ *
+ */
 public class Main {
-	private static final int TILE_WIDTH = 40;
-	private static final int TILE_HEIGHT = 30;
+	public static final int TILE_WIDTH = 40;
+	public static final int TILE_HEIGHT = 30;
 	
 	public static void main(String[] args) {
-		int w = TILE_WIDTH;
-		int h = TILE_HEIGHT;
-		if (args.length == 2) {
-			// use default values
-			w = Integer.parseInt(args[0]);
-			h = Integer.parseInt(args[1]);
-		}
-		JFrame le = new LevelEditor(w, h);
+		JFrame le = new LevelEditor();
 		le.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		le.setPreferredSize(new Dimension(600, 500));
 		le.pack();
@@ -41,26 +39,45 @@ public class Main {
 
 @SuppressWarnings("serial")
 class LevelEditor extends JFrame {
-
+	
+	// This is the path to the resources directory in our zamboni code
+	public static final String PATH = "ZealousZamboni/res/";
+	
+	// GUI strings
+	private static final String FILEMSG = "enter filename here";
+	private static final String FILE_ERR = "Filename error!";
+	private static final String FILENAME_INVALID = "Invalid filename: ";
+	private static final String FILE_EXISTS = "This file already exists: ";
+	private static final String TRY_AGAIN = "\nPlease try again.";
 	
 	private static final Color WALL = Color.BLACK;
 	private static final Color ICE = Color.WHITE;
 	private static final Color BLOCK = Color.RED;
 	private static final Color SKATER_ENTRANCE = Color.GREEN;
 	
-	private static final String FILENAME = "enter filename here";
-	
+	private int tileWidth;
+	private int tileHeight;
 	private JButton[] buttonArray;
 	private JTextField filenameField;
 	
-	public LevelEditor(final int tileWidth, final int tileHeight) {
-		
+	public LevelEditor() {
+		String width = (String) JOptionPane.showInputDialog(this, "Please enter tilemap width: ", "Tilemap Width", 
+				JOptionPane.PLAIN_MESSAGE, null, null, "40");
+		String height = (String) JOptionPane.showInputDialog(this, "Please enter tilemap height: ", "Tilemap Height", 
+				JOptionPane.PLAIN_MESSAGE, null, null, "30");
+		try {
+			tileWidth = Integer.parseInt(width);
+			tileHeight = Integer.parseInt(height);
+		} catch (NumberFormatException e) {
+			tileWidth = Main.TILE_WIDTH;
+			tileHeight = Main.TILE_HEIGHT;
+		}
 		// add color labels
 		JPanel colorLabels = new JPanel();
 		colorLabels.setLayout(new GridLayout(1, 4));
 		colorLabels.add(new JLabel("Wall  = BLACK"));
 		colorLabels.add(new JLabel("Ice   = WHITE"));
-		colorLabels.add(new JLabel("Block = Red"));
+		colorLabels.add(new JLabel("Block = RED"));
 		colorLabels.add(new JLabel("Entrance = GREEN"));
 		this.add(colorLabels, BorderLayout.NORTH);
 		
@@ -68,35 +85,49 @@ class LevelEditor extends JFrame {
 		JPanel donePanel = new JPanel();
 		JButton doneButton = new JButton("Done!");
 		doneButton.addActionListener(new ActionListener() {
+			
+			/* When the user is done, a CSV file is generated with the
+			 * tilemap info specified on the GUI
+			 */
 			public void actionPerformed(ActionEvent e) {
-				StringBuilder sb = new StringBuilder();
-				for (int i = 0, numTiles = tileWidth*tileHeight; i < numTiles; ++i) {
-					sb.append(getColorNum(buttonArray[i]));
-					if ((i + 1) % tileWidth == 0)
-						sb.append("\n");
-					else
-						sb.append(", ");
-				}
-				String csv = sb.toString();
-				String filename = filenameField.getText();
-				if (filename == null || filename.equals(FILENAME)) {
-					filename = "temp.txt";
-				} else if (!filename.endsWith(".txt")) {
-					filename += ".txt";
-				}
 				
-				Writer w = null;
-				try {
-					w = new BufferedWriter(new OutputStreamWriter(
-							new FileOutputStream(filename), "utf-8"), csv.length());
-					w.write(csv);
-				} catch (IOException ioe) {
-					ioe.printStackTrace();
-				} finally {
+				// First, make sure filename is valid and doesn't already exist
+				String filename = filenameField.getText();
+				String fileErr = filename;	// copy of orig name for error messages
+				File f = null;
+				if ((filename = checkFilename(filename)) == null) {
+					// User did not enter a valid filename
+					String message = FILENAME_INVALID + "\"" + fileErr + "\"" + TRY_AGAIN;
+					JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+				} else if ((f = checkValidFile(filename)) == null) {
+					// Filename already exists
+					String message = FILE_EXISTS + "\"" + fileErr + "\"" + TRY_AGAIN;
+					JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+				} else {
+					// Everything is good, go ahead and write csv file
+					StringBuilder sb = new StringBuilder();
+					for (int i = 0, numTiles = tileWidth*tileHeight; i < numTiles; ++i) {
+						sb.append(getColorNum(buttonArray[i]));
+						if ((i + 1) % tileWidth == 0)
+							sb.append("\n");
+						else
+							sb.append(", ");
+					}
+					String csv = sb.toString();
+					
+					Writer w = null;
 					try {
-						w.close();
-					} catch (Exception ex) {
-						ex.printStackTrace();
+						w = new BufferedWriter(new OutputStreamWriter(
+								new FileOutputStream(f), "utf-8"), csv.length());
+						w.write(csv);
+					} catch (IOException ioe) {
+						ioe.printStackTrace();
+					} finally {
+						try {
+							w.close();
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						}
 					}
 				}
 			}
@@ -104,7 +135,7 @@ class LevelEditor extends JFrame {
 		donePanel.add(doneButton, BorderLayout.LINE_END);
 		
 		filenameField = new JTextField();
-		filenameField.setText(FILENAME);
+		filenameField.setText(FILEMSG);
 		donePanel.add(filenameField, BorderLayout.LINE_START);
 		this.add(donePanel, BorderLayout.SOUTH);
 		
@@ -144,6 +175,36 @@ class LevelEditor extends JFrame {
 			buttonArray[i] = next;
 		}
 		this.add(buttonPanel, BorderLayout.CENTER);
+	}
+	
+	/**
+	 * Returns a File with name 'filename' if one with that filename
+	 * does not already exist.
+	 * @param filename
+	 * @return
+	 */
+	private File checkValidFile(String filename) {
+		File f = new File(filename);
+		if (f.exists()) {
+			f = null;
+		}
+		return f;
+	}
+	
+	/**
+	 * Returns the string with the correct path and extension
+	 * if 'filename' is valid, otherwise returns null.
+	 * @param filename
+	 * @return
+	 */
+	private String checkFilename(String filename) {
+		if (filename != null && !filename.equals(FILEMSG) && !filename.equals("")) {
+			filename = PATH + filename;
+			if (!filename.endsWith(".txt"))
+				filename += ".txt";
+			return filename;
+		}
+		return null;
 	}
 	
 	private int getColorNum(JButton b) {
