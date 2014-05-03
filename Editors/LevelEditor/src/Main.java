c0b0002d5d4c907bf34ee96f478bfbddcea587c6
danava04 <danava04@cs.washington.edu>
2014-05-03 05:08:49 -0700

    Added editing functionality to the level editor

--- Editors/LevelEditor/src/Main.java
+++ Editors/LevelEditor/src/Main.java
@@ -4,12 +4,17 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -41,13 +46,14 @@ public class Main {
 class LevelEditor extends JFrame {
 	
 	// This is the path to the resources directory in our zamboni code
-	public static final String PATH = "../ZealousZamboni/res/";
+	public static final String PATH = "../../ZealousZamboni/res/";
 	
 	// GUI strings
 	private static final String FILEMSG = "enter filename here";
 	private static final String FILE_ERR = "Filename error!";
 	private static final String FILENAME_INVALID = "Invalid filename: ";
 	private static final String FILE_EXISTS = "This file already exists: ";
+	private static final String FILE_DNE = "This file does not exist: ";
 	private static final String TRY_AGAIN = "\nPlease try again.";
 	
 	private static final Color WALL = Color.BLACK;
@@ -57,20 +63,56 @@ class LevelEditor extends JFrame {
 	
 	private int tileWidth;
 	private int tileHeight;
+	private File editFile;
 	private JButton[] buttonArray;
 	private JTextField filenameField;
 	
 	public LevelEditor() {
-		String width = (String) JOptionPane.showInputDialog(this, "Please enter tilemap width: ", "Tilemap Width", 
-				JOptionPane.PLAIN_MESSAGE, null, null, "40");
-		String height = (String) JOptionPane.showInputDialog(this, "Please enter tilemap height: ", "Tilemap Height", 
-				JOptionPane.PLAIN_MESSAGE, null, null, "30");
-		try {
-			tileWidth = Integer.parseInt(width);
-			tileHeight = Integer.parseInt(height);
-		} catch (NumberFormatException e) {
-			tileWidth = Main.TILE_WIDTH;
-			tileHeight = Main.TILE_HEIGHT;
+		// yes = 0, no = 1
+		int n = JOptionPane.showConfirmDialog(this, "Would you like to edit an existing tilemap?", "", JOptionPane.YES_NO_CANCEL_OPTION);
+		if (n == 0) {	// yes
+			File f = null;
+			while (f == null) {
+				String s = (String) JOptionPane.showInputDialog(this, 
+						"Enter the filename of the file you want to edit "
+						+ "(do not include the path to the resources folder)", 
+						"Open file", JOptionPane.PLAIN_MESSAGE, null, null, "");
+				if (s != null && s != "") {
+					s = PATH + s;
+					f = new File(s);
+					if (!f.exists()) {
+						f = null;
+						if (!s.endsWith(".txt")) {
+							s += ".txt";
+							f = new File(s);
+							if (!f.exists()) {
+								String message = FILE_DNE + "\"" + s + "\"" + TRY_AGAIN;
+								JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+								f = null;
+							}
+						}
+					}
+				} else {
+					String message = FILENAME_INVALID + "\"" + s + "\"" + TRY_AGAIN;
+					JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			editFile = f;
+		} else if (n == 1) {		// no
+			editFile = null;
+			String width = (String) JOptionPane.showInputDialog(this, "Enter tilemap width: ", "Tilemap Width", 
+					JOptionPane.PLAIN_MESSAGE, null, null, "40");
+			String height = (String) JOptionPane.showInputDialog(this, "Enter tilemap height: ", "Tilemap Height", 
+					JOptionPane.PLAIN_MESSAGE, null, null, "30");
+			try {
+				tileWidth = Integer.parseInt(width);
+				tileHeight = Integer.parseInt(height);
+			} catch (NumberFormatException e) {
+				tileWidth = Main.TILE_WIDTH;
+				tileHeight = Main.TILE_HEIGHT;
+			}
+		} else {
+			System.exit(0);
 		}
 		// add color labels
 		JPanel colorLabels = new JPanel();
@@ -90,90 +132,99 @@ class LevelEditor extends JFrame {
 			 * tilemap info specified on the GUI
 			 */
 			public void actionPerformed(ActionEvent e) {
-				
-				// First, make sure filename is valid and doesn't already exist
-				String filename = filenameField.getText();
-				String fileErr = filename;	// copy of orig name for error messages
-				File f = null;
-				if ((filename = checkFilename(filename)) == null) {
-					// User did not enter a valid filename
-					String message = FILENAME_INVALID + "\"" + fileErr + "\"" + TRY_AGAIN;
-					JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
-				} else if ((f = checkValidFile(filename)) == null) {
-					// Filename already exists
-					String message = FILE_EXISTS + "\"" + fileErr + "\"" + TRY_AGAIN;
-					JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+				if (editFile == null) {
+					// First, make sure filename is valid and doesn't already exist
+					String filename = filenameField.getText();
+					String fileErr = filename;	// copy of orig name for error messages
+					File f = null;
+					if ((filename = checkFilename(filename)) == null) {
+						// User did not enter a valid filename
+						String message = FILENAME_INVALID + "\"" + fileErr + "\"" + TRY_AGAIN;
+						JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+					} else if ((f = checkValidFile(filename)) == null) {
+						// Filename already exists
+						String message = FILE_EXISTS + "\"" + fileErr + "\"" + TRY_AGAIN;
+						JOptionPane.showMessageDialog(LevelEditor.this, message, FILE_ERR, JOptionPane.ERROR_MESSAGE);
+					} else {
+						// Everything is good, go ahead and write csv file
+						prepAndWriteFile(f);
+					}
 				} else {
-					// Everything is good, go ahead and write csv file
-					StringBuilder sb = new StringBuilder();
-					for (int i = 0, numTiles = tileWidth*tileHeight; i < numTiles; ++i) {
-						sb.append(getColorNum(buttonArray[i]));
-						if ((i + 1) % tileWidth == 0)
-							sb.append("\n");
-						else
-							sb.append(", ");
-					}
-					String csv = sb.toString();
-					
-					Writer w = null;
-					try {
-						w = new BufferedWriter(new OutputStreamWriter(
-								new FileOutputStream(f), "utf-8"), csv.length());
-						w.write(csv);
-					} catch (IOException ioe) {
-						ioe.printStackTrace();
-					} finally {
-						try {
-							w.close();
-						} catch (Exception ex) {
-							ex.printStackTrace();
-						}
-					}
+					prepAndWriteFile(editFile);
 				}
 			}
 		});
-		donePanel.add(doneButton, BorderLayout.LINE_END);
 		
-		filenameField = new JTextField();
-		filenameField.setText(FILEMSG);
-		donePanel.add(filenameField, BorderLayout.LINE_START);
-		this.add(donePanel, BorderLayout.SOUTH);
-		
-		// add ALL of the buttons
+		// button panel to hold all of the buttons
 		JPanel buttonPanel = new JPanel();
 		
-		int numTiles = tileWidth*tileHeight;
-		buttonArray = new JButton[numTiles];
-		buttonPanel.setLayout(new GridLayout(tileHeight, tileWidth));
-		for (int i = 0; i < numTiles; ++i) {
-			JButton next = new JButton();
+		
+		if (editFile == null) {
 			
-			//Preset edges to be walls
-			if ((i < tileWidth) || (i >= numTiles - tileWidth) ||
-				(i % tileWidth == 0) || ((i+1) % tileWidth == 0)) {
-				next.setBackground(WALL);
-			} else {
-				next.setBackground(ICE);
-			}
+			// done panel
+			filenameField = new JTextField();
+			donePanel.add(doneButton, BorderLayout.LINE_END);
+			donePanel.add(filenameField, BorderLayout.LINE_START);
+			this.add(donePanel, BorderLayout.SOUTH);
+			filenameField.setText(FILEMSG);
 			
-			// add action listener to change color
-			next.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					JButton clicked = (JButton) e.getSource();
-					if (clicked.getBackground().equals(WALL)) {
-						clicked.setBackground(ICE);
-					} else if (clicked.getBackground().equals(ICE)) {
-						clicked.setBackground(BLOCK);
-					} else if (clicked.getBackground().equals(BLOCK)) {
-						clicked.setBackground(SKATER_ENTRANCE);
-					} else {	// skater entrance
-						clicked.setBackground(WALL);
-					}
+			int numTiles = tileWidth*tileHeight;
+			buttonArray = new JButton[numTiles];
+			buttonPanel.setLayout(new GridLayout(tileHeight, tileWidth));
+			for (int i = 0; i < numTiles; ++i) {
+				JButton next = new JButton();
+				
+				//Preset edges to be walls
+				if ((i < tileWidth) || (i >= numTiles - tileWidth) ||
+					(i % tileWidth == 0) || ((i+1) % tileWidth == 0)) {
+					next.setBackground(WALL);
+				} else {
+					next.setBackground(ICE);
 				}
-			});
-			buttonPanel.add(next);
-			buttonArray[i] = next;
+				
+				// add action listener to change color
+				next.addActionListener(new GridButtonListener());
+				buttonPanel.add(next);
+				buttonArray[i] = next;
+			}
+		} else {
+			// done panel (done button only)
+			this.add(doneButton, BorderLayout.SOUTH);
+			BufferedReader r = null;
+			try {
+				r = new BufferedReader(new InputStreamReader(
+						new FileInputStream(editFile), "utf-8"));
+				List<String> rows = new ArrayList<String>();
+				
+				String s = null;
+				while ((s = r.readLine()) != null) {
+					rows.add(s);
+				}
+				if (rows.isEmpty()) {
+					r.close();
+					throw new IllegalStateException("File " + editFile.getName() + " is empty");
+				}
+				tileHeight = rows.size();
+				String[] tiles = rows.get(0).split(", ");
+				tileWidth = tiles.length;
+				buttonArray = new JButton[tileWidth * tileHeight];
+				buttonPanel.setLayout(new GridLayout(tileHeight, tileWidth));
+				int buttonIdx = 0;
+				setButtonRow(tiles, buttonPanel, buttonIdx);
+				for (int i = 1; i < rows.size(); ++i) {
+					buttonIdx = i * tileWidth;
+					tiles = rows.get(i).split(", ");
+					setButtonRow(tiles, buttonPanel, buttonIdx);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					r.close();
+				} catch (Exception ee){}
+			}
 		}
+		
 		this.add(buttonPanel, BorderLayout.CENTER);
 	}
 	
@@ -216,6 +267,70 @@ class LevelEditor extends JFrame {
 			return 2;
 		} else {
 			return 3;
+		}
+	}
+	
+	private Color getColor(int c) {
+		if (c == 0) {
+			return ICE;
+		} else if (c == 1) {
+			return WALL;
+		} else if (c == 2) {
+			return BLOCK;
+		} else {
+			return SKATER_ENTRANCE;
+		}
+	}
+	
+	private void setButtonRow(String[] tiles, JPanel buttonPanel, int buttonIdx) {
+		for (int i = 0; i < tileWidth; ++i) {
+			JButton next = new JButton();
+			next.setBackground(getColor(Integer.parseInt(tiles[i])));
+			next.addActionListener(new GridButtonListener());
+			buttonPanel.add(next);
+			buttonArray[buttonIdx + i] = next;
+		}
+	}
+	
+	private void prepAndWriteFile(File f) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0, numTiles = tileWidth*tileHeight; i < numTiles; ++i) {
+			sb.append(getColorNum(buttonArray[i]));
+			if ((i + 1) % tileWidth == 0)
+				sb.append("\n");
+			else
+				sb.append(", ");
+		}
+		String csv = sb.toString();
+		
+		Writer w = null;
+		try {
+			w = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(f, false), "utf-8"), csv.length());
+			w.write(csv);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				w.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	class GridButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton clicked = (JButton) e.getSource();
+			if (clicked.getBackground().equals(WALL)) {
+				clicked.setBackground(ICE);
+			} else if (clicked.getBackground().equals(ICE)) {
+				clicked.setBackground(BLOCK);
+			} else if (clicked.getBackground().equals(BLOCK)) {
+				clicked.setBackground(SKATER_ENTRANCE);
+			} else {	// skater entrance
+				clicked.setBackground(WALL);
+			}
 		}
 	}
 }
